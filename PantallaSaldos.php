d89c0d4761d6471f575b6d7ca1de4a8f8afde986
dariobada <63989105+dariobada@users.noreply.github.com>
2020-10-24 22:32:25 -0300

    Add files via upload

---
+++ PantallaSaldos.php
@@ -0,0 +1,37 @@
+<?php
+
+// controllers/PantallaSaldos.php
+	
+	require '../fw/fw.php';
+	require '../models/Cuentas.php';
+	//require '../views/ListadoCuentas.php';
+
+
+
+	var_dump("hola1");
+	session_start();
+	var_dump("hola2");
+
+	$c = new Cuentas();
+	var_dump("hola3");
+	/////////!!!!!!!!!!!! HAY QUE VALIDAR QUE VENGA EL USUARIO !!!!!!!!!!////////////////////
+	$cuentasUsua = $c->getCuentasPorUsuario($_SESSION['IdUsuario']);
+	var_dump("hola4");
+	//en este array guardamos las cuentas y sus detalles
+	$aux = array();
+	var_dump("hola5");
+	$i = 0;
+
+	foreach($cuentasUsua as $cu){
+		//$aux[i] = $c->getDetalleDeCuenta($cuentasUsua['id_usuario'][i]);
+		var_dump($cuentasUsua['id_usuario'][i]);
+
+		i++;
+
+	}
+
+	
+	
+
+
+?>
