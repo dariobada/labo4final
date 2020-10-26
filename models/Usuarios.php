76894c6f3f8830f1cc7aad6e39db63b6298569cd
dariobada <63989105+dariobada@users.noreply.github.com>
2020-10-26 10:16:41 -0300

    Add files via upload

--- models/Usuarios.php
+++ models/Usuarios.php
@@ -9,7 +9,7 @@
 		public function getUsuario($usua){
 
 			$cons = "'" . $usua . "'";
-			$sentencia = 'SELECT * FROM public."USUARIOS" WHERE "nombre_usuario" = ';
+			$sentencia = 'SELECT * FROM public."USUARIOS" WHERE "id_login_usuario" = ';
 
 			$this->db->query($sentencia . $cons);
 			return $this->db->fetch();
