61b4248c22c61b8e92e7af5888a97262cca39a96
dariobada <63989105+dariobada@users.noreply.github.com>
2020-10-28 14:04:29 -0300

    Add files via upload

--- html/ListadoCuentas.php
+++ html/ListadoCuentas.php
@@ -124,7 +124,7 @@
 			<div class="menuSuperior">
 				<div class="headerOpcionImpar"><img src="../logo.png"></div>
 				<div class="headerOpcionPar" id="divConsultaSaldos">Consulta de saldos</div>
-			<!--	<div class="headerOpcionImpar" id="divTarjetas">Tarjetas</div>-->
+				<div class="headerOpcionImpar" id="divTarjetas">Tarjetas</div>
 				<div class="headerOpcionPar">Transferencias</div>
 				<div class="headerOpcionImpar" id="divCerrarSesion">Cerrar sesión</div>
 			</div>
