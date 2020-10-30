37e09590357a3faf0cfc1a3da7bb56756d743ed6
dariobada <63989105+dariobada@users.noreply.github.com>
2020-10-30 14:18:48 -0300

    Add files via upload

--- models/TipoCuentas.php
+++ models/TipoCuentas.php
@@ -11,8 +11,10 @@
 			$cons = "'" . $idTipoCuenta . "'";
 			$sentencia = 'SELECT * FROM public."TIPO_CUENTAS" WHERE "id_tipo_cuenta" = ';
 
+			var_dump($idTipoCuenta);
 			$this->db->query($sentencia . $cons);
 			$ret = $this->db->fetch();
+			var_dump($ret);
 			return $ret['desc_tipo_cuenta'];
 
 		}
