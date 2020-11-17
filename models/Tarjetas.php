e9f28c5234b24f66fa54f86a800320b955506174
dariobada <63989105+dariobada@users.noreply.github.com>
2020-11-16 23:15:56 -0300

    Add files via upload

--- models/Tarjetas.php
+++ models/Tarjetas.php
@@ -24,6 +24,16 @@
 
 		}
 
+		public function getDetalleTarjetaPorNumero($nroTarj){
+
+			$tarjeta = "'" . $nroTarj . "'";
+			$sentencia = 'SELECT * FROM public."TARJETAS" WHERE "nro_tarjeta" = ';
+
+			$this->db->query($sentencia . $tarjeta);
+			return $this->db->fetch();
+
+		}
+
 		public function getDetalleExtension($idTarj){
 
 			$sentencia = 'SELECT * FROM public."EXTENSIONES_TARJETA" WHERE "id_tarjeta_extension" = ';
@@ -55,19 +65,33 @@
 			$this->db->query($sentencia);
 
 			$maximo = $this->db->fetch();
+			$nroTarjetaExtension = $maximo["max"] + "1";
 
 			//obtengo los datos que faltan para dar de alta la nueva tarjeta
 			$datosFaltantes = $this->getDetalleTarjeta($idTarj);
 
 			//realizo el insert de la nueva tarjeta
-			$nroTarjeta = "'" . ($maximo["max"] + "1") . "'";
+			$nroTarjeta = "'" . $nroTarjetaExtension . "'";
 			$tipoTarjeta = "'" . "E" . "'";
 			$codProveedor =  "'" . $datosFaltantes['cod_proveedor'] . "'" ;
 			$codEstado = "'" . "A" . "'";
 			$sentencia = 'INSERT INTO public."TARJETAS"( nro_tarjeta, tipo_tarjeta, cod_proveedor, fecha_alta, cod_estado)
 	                           VALUES (' . $nroTarjeta . ', ' . $tipoTarjeta . ', ' . $codProveedor . ', CURRENT_DATE, ' . $codEstado . ')';
-
 	        $this->db->query($sentencia);
+
+	        //se vincula la extensión a la tarjeta principal
+	        $idExtension = $this->getDetalleTarjetaPorNumero($nroTarjetaExtension);
+
+	        $nom = "'" . $nombre . "'";
+	        $ape = "'" . $apellido . "'";
+	        $sentencia = 'INSERT INTO public."EXTENSIONES_TARJETA" 
+	        VALUES (' . $idTarj . ', ' . $idExtension['id_tarjeta'] . ', CURRENT_DATE, null, ' . $codEstado . ', ' . $nom . ', ' . $ape . ', ' . $nroDocumento . ')';
+	        var_dump("sentencia: " . $sentencia);
+	        $this->db->query($sentencia);
+
+	        //se vincula la nueva tarjeta a la persona
+
+	        
 			
 			//var_dump($datosFaltantes);
 			//var_dump($maximo["max"] + "1");
