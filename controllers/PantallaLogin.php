6189a9b7ff53f6091513766a3614244430ee1134
dariobada <63989105+dariobada@users.noreply.github.com>
2020-11-17 16:40:49 -0300

    Add files via upload

--- controllers/PantallaLogin.php
+++ controllers/PantallaLogin.php
@@ -85,7 +85,7 @@
 					$_SESSION['logueado'] = true;
 					$_SESSION['IdUsuario'] = $usuario['id_usuario'];
 					$_SESSION['nombre'] = $_POST['usuario'];
-					header("Location: PantallaSaldos.php");
+					//header("Location: PantallaSaldos.php");
 					exit();
 
 				}
@@ -95,7 +95,7 @@
 					$_SESSION['logueado'] = true;
 					$_SESSION['IdUsuario'] = $usuario['id_usuario'];
 					$_SESSION['nombre'] = $_POST['usuario'];
-					header("Location: PantallaTarjetas.php");
+					//header("Location: PantallaTarjetas.php");
 					exit();
 				}
 
