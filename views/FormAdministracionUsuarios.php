25ce0c317e373d06e2089f96bca00ee9a448dc16
dariobada <63989105+dariobada@users.noreply.github.com>
2020-11-19 11:22:18 -0300

    Add files via upload

---
+++ views/FormAdministracionUsuarios.php
@@ -0,0 +1,18 @@
+<?php
+
+
+	// views/FormAdministracionUsuarios.php
+
+	class FormAdministracionUsuarios extends View {
+
+		public $usuarios;
+		public $proveedores;
+		public $tarjetas;
+		public $mensaje;
+
+		
+
+
+	}
+
+?>
