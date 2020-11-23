81b5753fab69edf594672f15845e41d5f38f1db6
dariobada <63989105+dariobada@users.noreply.github.com>
2020-11-23 16:48:02 -0300

    Add files via upload

---
+++ views/FormProductos.php
@@ -0,0 +1,17 @@
+<?php
+
+
+	// views/FormProductos.php
+
+	class FormProductos extends View {
+
+		public $cuentas;
+		public $usuario;
+		public $operaCuentas;
+		public $operaTarjetas;
+		
+
+
+	}
+
+?>
