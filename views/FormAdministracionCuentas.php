c997af84f2bf312b3504e0f882e9a051c7e658ba
dariobada <63989105+dariobada@users.noreply.github.com>
2020-11-23 10:20:13 -0300

    Add files via upload

---
+++ views/FormAdministracionCuentas.php
@@ -0,0 +1,18 @@
+<?php
+
+
+	// views/FormAdministracionCuentas.php
+
+	class FormAdministracionCuentas extends View {
+
+		public $usuarios;
+		public $tipoCuentas;
+		public $cuentas;
+		public $mensaje;
+
+		
+
+
+	}
+
+?>
