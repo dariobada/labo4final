421e349ec4e1dc6d04b1cf534906f3906cf38ad0
dariobada <63989105+dariobada@users.noreply.github.com>
2020-12-08 09:51:21 -0300

    Add files via upload

--- html/FormAdministracionCuentas.php
+++ html/FormAdministracionCuentas.php
@@ -241,7 +241,7 @@
 					<form action="" method="post" id="formEliminar">
 						<h2>Baja cuentas</h2>
 						<label>Seleccionar cuenta: </label>
-							<select class="cuenta" name="cuenta" required="required" id="cuentaBaja" onchange="llamar()"> 
+							<select class="cuenta" name="cuenta" required="required" id="cuenta"> 
 								<?php 
 									foreach ($this->cuentas as $cu){
 										echo '<option value="' . $cu['id_cuenta'] . '">' . $cu['tipo_cuenta'] . ' ' . $cu['nro_cuenta'] . ' (' . $cu['saldo_moneda'] . ')</option>';
@@ -314,10 +314,11 @@
 
 			$(document).ready(function(){
 
-				function llamar() {
-				  alert(document.getElementById("cuentaBaja").value);
-				  
-				};
+				$("#usuario").change(function () {
+					
+					alert("hola");
+
+				});
 
 				
 				$("#divCuentas").click(function(){
